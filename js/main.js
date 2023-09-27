600d18d9b1748ba35652d503f2d42cba2db8c559
kimkihye <yunej170@gmail.com>
2023-09-27 18:25:42 +0900

    marvel2

--- js/main.js
+++ js/main.js
@@ -34,47 +34,5 @@ closes.forEach((el, index) => {
     })
 })
 
-// //youtube------------------------------------------------------
-// //key : AIzaSyCnc0X2GA589D0hjsq9tTEbfvvPLHddpw0
-// //list : PLwCWtKkR2SGMCgiaQKRtajpNf7bSUaihP
 
-// let key = "AIzaSyCnc0X2GA589D0hjsq9tTEbfvvPLHddpw0";
-// let list = "PLwCWtKkR2SGMCgiaQKRtajpNf7bSUaihP";
-// let num = 5;
 
-// let vid = marvel.querySelector("article .character .vid");
-// const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${list}&maxResults=${num}`;
-
-// fetch(url)
-//     .then((data)=>{
-//         return data.json();
-//     })
-//     .then((json)=>{
-//         let items = json.items;
-
-//         let result = "";
-
-//         items.map((el)=>{
-//             result += `
-//                 <artlcle>
-//                     <a href="${el.snippet.resourceId.videoId}">
-//                         <img src="${el.snippet.thumbnails.medium.url}">
-//                     </a>
-//                     <h1>${el.snippet.title}</h1>
-//                 </artlcle>
-//             `;
-//         })
-//         vid.innerHTML = result;
-//     })
-
-//     vid.addEventListener("click",()=>{
-//         e.preventDefault();
-
-//         let vidId = e.target.closest("article").querySelector("a").getAttribute("href");
-
-//         let pop = document.createElement("figure");
-//         pop.innerHTML = `
-//             <iframe src="https://www.youtube.com/embed/${vidId}">
-//         `;
-//         vid.append(pop);
-//     })
